4e2baf3c3d4e188218f27de2e6fb279f596a5ad1
betonk26 <betonk26@gmail.com>
2023-03-09 16:53:54 +0700

    set data view product

--- resources/views/frontend/view.blade.php
+++ resources/views/frontend/view.blade.php
@@ -1,15 +1,26 @@
 @extends('layouts.app')
 @section('content')
-
     <div class="container py-2">
         {{-- main content --}}
         <div class="field p-4">
             <div class="row mt-4">
-                <div class="col-6 align-self-center">
-                    <h1><b>The Most Complete Place To Meet Hobbyist Needs</b></h1>
-                    <a href="#barang" class="btn btn-dark text-primary rounded-pill">Buy Now!</a>
+                <div class="col-md-6">
+                    <img src="{{ asset('uploads/barang/' . $item->gambar) }}" alt="" width="100%" height="100%">
                 </div>
-                <div class="col-6 align-self-end"><img src="{{ asset('./img/eva-01.svg') }}" alt="eva-01"></div>
+                <div class="col-md-6 align-self-top">
+                    <form action="">
+                        <h2>{{ $item->nama }}</h2>
+                        <h2>{{ $item->harga }}</h2>
+                        <h2>Spesifikasi</h2>
+                        <h4>{{ $item->desc }}</h4>
+                        <h2>informasi</h2>
+                        <p>Serial : {{ $item->anime }}</p>
+                        <p>kategori : {{ $item->kategori->kategori_id }}</p>
+                        <p>jumlah : {{ $item->jumlah }}</p>
+                        <button>Pre-Order</button>
+                        <button>Cart</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
