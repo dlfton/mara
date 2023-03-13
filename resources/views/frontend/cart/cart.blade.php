c8700fab9bac1226c30287b3c8e16f7aa9df94ed
betonk26 <betonk26@gmail.com>
2023-03-13 18:11:18 +0700

    add livewire

--- resources/views/frontend/cart/cart.blade.php
+++ resources/views/frontend/cart/cart.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.app')
 
 @section('title', 'cart')
-    
 
 @section('content')
+    @livewireScripts
     <div class="container">
         <h1>Welcome</h1>
     </div>
+    @livewireScripts
 @endsection
